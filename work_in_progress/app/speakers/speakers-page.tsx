338bb753245d09bc8485d4ab6cdc651552a66593
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-06-19 15:45:37 +0100

    adding zod schema

--- work_in_progress/app/speakers/speakers-page.tsx
+++ work_in_progress/app/speakers/speakers-page.tsx
@@ -4,6 +4,7 @@ import { SpeakerMenuProvider } from "@/app/speakers/speaker-menu-context";
 import { getSpeakersFromCache } from "@/speakers-cache-service";
 import { SpeakersDataProvider } from "@/app/speakers/speakers-data-context";
 import {Speaker} from "@/app/types/Speaker";
+import {speakerSchema} from "@/app/types/Speaker";
 
 async function loadSpeakersData() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -12,10 +13,18 @@ async function loadSpeakersData() {
 
 export default async function SpeakersPage() {
   const speakersData = await loadSpeakersData();
+  let speakersParsed
+  try {
+      speakersParsed = speakerSchema.array().parse(speakersData)
+  } catch (err: unknown) {
+      throw new Error("invalid data " +
+          (err instanceof Error ? err.message : "")
+      )
+  }
   return (
     <SpeakerMenuProvider>
       <SpeakerMenu />
-      <SpeakersDataProvider initialData={speakersData}>
+      <SpeakersDataProvider initialData={speakersParsed} >
         <SpeakersList />
       </SpeakersDataProvider>
     </SpeakerMenuProvider>
